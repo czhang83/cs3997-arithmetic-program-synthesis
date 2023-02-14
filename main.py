ae1f1cd8528ee0cc1b17e846248012852816cede
Cynthia Zhang <cz2696@columbia.edu>
2023-02-14 16:13:00 -0500

    Implemented brute force algorithm
    won't terminate since search space is large

--- main.py
+++ main.py
@@ -4,23 +4,49 @@
 # This return multiple functions that can produce the output from the input, in the format of "+4" for example
 # the possible solutions are tested from an increasing order, from 0 to MAX_BOUND
 # and in the order of integer division, multiplication, subtraction, and addition
+# TODO fix comments
 
 
 # largest possible integer
 MAX_BOUND = 10
+OPERATORS = ['+', '-', '*', '/']
+NUMBERS = []
+for i in range(MAX_BOUND):
+    NUMBERS.append(str(i))
+
+
+def append_operator(formula):
+    results = []
+    for n in NUMBERS:
+        for operator in OPERATORS:
+            # avoid division by 0
+            # avoid +/- 0 since this does nothing
+            if (operator == '/' or operator == '+' or operator == '-') and n == '0':
+                continue
+            result = formula + operator + n
+            results.append(result)
+    return results
 
 def synthesis(x, y):
     results = []
-    for i in range(10):
-        if i != 0 and x / i == y:
-            results.append('/' + str(i))
-        if x * i == y:
-            results.append('*' + str(i))
-        if x - i == y:
-            results.append('-' + str(i))
-        if x + i == y:
-            results.append('+' + str(i))
-    return results
+    results.append(str(x))
+
+    counter = 0
+    while counter < 4:
+        new_results = []
+        for r in results:
+            if eval(r) == y:
+                print(r)
+            new_results = new_results + append_operator(r)
+        results = new_results
+        counter = counter + 1
+
+    valid_result = []
+    for r in results:
+        if eval(r) == y:
+            valid_result.append(r)
+
+    return valid_result
 
 
 # Press the green button in the gutter to run the script.
@@ -31,8 +57,6 @@ if __name__ == '__main__':
 
         results = synthesis(x, y)
         for result in results:
-            print(result)
-            str_to_eval = str(x) + str(result)
-            print("eval(" + str_to_eval + ")=" + str(eval(str_to_eval)))
+            print(result + ")=" + str(eval(result)))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
